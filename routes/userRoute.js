caa61dd4f28683549809ace976a8ae2a1d635aa8
ANHHOAI2739 <anhhoai2739@gmail.com>
2024-01-02 21:11:52 +0700

    1/2

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,20 @@ import {
   authMiddleware,
   isAdminMiddleware,
 } from '../middlewares/authMiddleware.js';
-import { allUser, setAdmin } from '../controllers/userController.js';
+import {
+  allUser,
+  deleteUser,
+  editUser,
+  searchUsers,
+  setAdmin,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, allUser);
 router.put('/setAdmin/:userId', authMiddleware, isAdminMiddleware, setAdmin);
+router.put('/profile/:userId', authMiddleware, editUser);
+router.get('/search/:userId', authMiddleware, searchUsers);
+router.put('/delete/:userId', authMiddleware, isAdminMiddleware, deleteUser);
 
 export default router;
